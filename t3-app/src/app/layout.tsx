c055966152b850ef1f5b4999e4d84d459803d8ab
Yufeng Zhao <yufeng-zhao@outlook.com>
2024-02-24 02:48:54 -0800

    load local font

--- t3-app/src/app/layout.tsx
+++ t3-app/src/app/layout.tsx
@@ -2,11 +2,30 @@ import "@/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
 import { TRPCReactProvider } from "@/trpc/react";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+const cotham = localFont({
+  src: "./fonts/Cotham-Sans-Regular.woff2",
+  display: "swap",
+  variable: "--font-cotham-sans",
+});
+
+const junicode = localFont({
+  src: "./fonts/JunicodeTwoBetaVF-Roman.woff2",
+  display: "swap",
+  variable: "--font-junicode",
+  declarations: [
+    {
+      prop: "font-stretch",
+      value: "75% 125%",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -22,7 +41,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body
+        className={`font-sans ${inter.variable} ${cotham.variable} ${junicode.variable}`}
+      >
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
