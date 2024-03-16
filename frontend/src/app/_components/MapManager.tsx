c8722e9fa904112a14d7087a8c5a4484a7675453
Yufeng Zhao <yufeng-zhao@outlook.com>
2024-03-16 16:42:06 -0400

    refactor drawer location

--- frontend/src/app/_components/MapManager.tsx
+++ frontend/src/app/_components/MapManager.tsx
@@ -3,6 +3,20 @@
 import { env } from "@/env";
 import React, { useEffect, useState, useCallback } from "react";
 
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+
+import { useBreakpoint } from "@/hooks/useBreakpoint";
+
 import type { Airport } from "@/utils/airport";
 
 import { INITIAL_VIEW_STATE } from "@/lib/map-config";
@@ -24,6 +38,12 @@ const MapManager = ({ allAirports }: MapManagerProps) => {
 
   const [initialViewState, setInitialViewState] = useState(INITIAL_VIEW_STATE);
 
+  const [lastClickedAirport, setLastClickedAirport] = useState<Airport | null>(
+    null,
+  );
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { isMd } = useBreakpoint("md");
+
   const flyToAirport = useCallback(
     (airport: Airport) => {
       setInitialViewState((prev) => ({
@@ -36,8 +56,10 @@ const MapManager = ({ allAirports }: MapManagerProps) => {
         transitionInterpolator: new FlyToInterpolator(),
         numberOfMutations: prev.numberOfMutations + 1,
       }));
+      setIsDrawerOpen(true);
+      setLastClickedAirport(airport);
     },
-    [setInitialViewState],
+    [setInitialViewState, setIsDrawerOpen, setLastClickedAirport],
   );
 
   useEffect(() => {
@@ -67,6 +89,73 @@ const MapManager = ({ allAirports }: MapManagerProps) => {
         initialViewState={initialViewState}
         flyToAirport={flyToAirport}
       />
+
+      <Drawer
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        onOpenChange={setIsDrawerOpen}
+        modal={false}
+        preventScrollRestoration={true}
+        dismissible={true}
+        direction={isMd ? "right" : "bottom"}
+      >
+        {lastClickedAirport && (
+          <DrawerContent className=" font-bitmap mx-auto max-w-2xl md:ml-auto md:mr-0">
+            <DrawerHeader>
+              <DrawerTitle className="text-center text-3xl md:text-5xl">
+                {lastClickedAirport.iata_code}
+              </DrawerTitle>
+              <DrawerDescription
+                className="scrollbar-thin scrollbar-thumb-retro-blue scrollbar-track-gray-200
+                   bor text-md max-h-[10rem]  min-h-0 overflow-y-scroll overscroll-none
+                   border-2 border-retro-cyan bg-white px-2 py-2 leading-tight text-black shadow-inner md:max-h-[35vh] md:min-h-[14rem] md:text-xl"
+              >
+                <div className="grid grid-cols-8 gap-2 text-left">
+                  <div className="col-span-3">Airport</div>
+                  <div className="col-span-5 ">
+                    <div className="font-semibold">
+                      {lastClickedAirport?.name} (
+                      {lastClickedAirport?.iso_country})
+                    </div>
+                    <div>{lastClickedAirport.type}</div>
+                  </div>
+
+                  {/* divider here */}
+                  <div className="col-span-8">
+                    <hr className="-mx-2 border-t-2 border-retro-gray" />
+                  </div>
+                  <div className="col-span-3">Filetype</div>
+                  <div className="col-span-5">
+                    {lastClickedAirport?.filetypes.map((filetype, i) => {
+                      return (
+                        <div key={i}>
+                          <div className="font-semibold">
+                            {filetype.description}
+                          </div>
+                          {filetype.used_by && (
+                            <div>Used by {filetype.used_by}</div>
+                          )}
+                          {i < lastClickedAirport.filetypes.length - 1 ? (
+                            <hr className="-mx-2 my-1 border-t-2 border-retro-gray" />
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </DrawerDescription>
+            </DrawerHeader>
+            {/* <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter> */}
+          </DrawerContent>
+        )}
+      </Drawer>
     </>
   );
 };
