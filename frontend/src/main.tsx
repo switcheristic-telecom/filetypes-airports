51e5ccc62730abc36a41ca00491949dd62a344a5
Yufeng Zhao <yufeng-zhao@outlook.com>
2026-03-23 13:26:59 -0400

    remove vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Analytics />
-  </>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
