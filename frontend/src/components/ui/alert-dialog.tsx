7009c8fd154d1488254364d02931c3a65c3071c0
Yufeng Zhao <yufeng-zhao@outlook.com>
2026-03-23 13:58:47 -0400

    fix animation

--- frontend/src/components/ui/alert-dialog.tsx
+++ frontend/src/components/ui/alert-dialog.tsx
@@ -17,8 +17,9 @@ const AlertDialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      "bg-retro-blue",
+      "fixed inset-0 z-50 bg-retro-blue",
+      "data-[state=open]:animate-[dialog-overlay-in_200ms_ease-out_forwards]",
+      "data-[state=closed]:animate-[dialog-overlay-out_200ms_ease-in_forwards]",
       className,
     )}
     {...props}
@@ -36,7 +37,9 @@ const AlertDialogContent = React.forwardRef<
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg gap-4 border bg-background p-6 shadow-lg",
+        "data-[state=open]:animate-[dialog-content-in_200ms_ease-out_forwards]",
+        "data-[state=closed]:animate-[dialog-content-out_200ms_ease-in_forwards]",
         className,
       )}
       {...props}
